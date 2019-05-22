f6901ae316e9e3a3761f7cf83a84e32f516d94aa
Mike Lock <michael.lockwitz@consensys.net>
2019-05-22 11:22:24 -0600

    Fixed ExampleCard component rendering in mdx.

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,7 +1,10 @@
 import React from "react";
 import mdxComponents from "./src/components/mdxComponents";
 import { MDXProvider } from "@mdx-js/react";
+import ExampleCard from "./src/components/documentation/ExampleCard";
 
 export const universalWrapRootElement = ({ element }) => (
-  <MDXProvider components={mdxComponents}>{element}</MDXProvider>
+  <MDXProvider components={{ mdxComponents, ExampleCard }}>
+    {element}
+  </MDXProvider>
 );
