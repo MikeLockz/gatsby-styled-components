f7d5263806d077eb093bf03c4d010bbca28130ae
Mike Lock <michael.lockwitz@consensys.net>
2019-05-22 12:36:33 -0600

    Fixed broken develop Button docs.

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,10 +1,7 @@
 import React from "react";
 import mdxComponents from "./src/components/mdxComponents";
 import { MDXProvider } from "@mdx-js/react";
-import ExampleCard from "./src/components/documentation/ExampleCard";
 
 export const universalWrapRootElement = ({ element }) => (
-  <MDXProvider components={{ mdxComponents, ExampleCard }}>
-    {element}
-  </MDXProvider>
+  <MDXProvider components={mdxComponents}>{element}</MDXProvider>
 );
